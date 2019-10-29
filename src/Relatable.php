2aaac6d10f9b48a9524e3387bf230b255921d554
Daniel Farias <daniel.dsfarias@gmail.com>
2019-10-29 11:51:12 -0300

    Adiciona Relatable

---
+++ src/Relatable.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Danieldsf\Searcher;
+
+use Illuminate\Support\Facades\DB;
+
+trait Relatable {
+
+    public function getRelatedQuery(){
+        $pieces = explode(" ", $this->name);
+
+        $query = self::select(DB::raw('id,name'))
+        ->where(function($query) use ($pieces){
+            $size = count($pieces);
+            for ($i = 0; $i < $size; $i++) {
+                $key = strtolower($pieces[$i]);
+                $query = $query->orWhere('name', 'ilike', "%$key%");
+            }
+        })
+        ->orderBy('name')
+        ->groupBy(DB::raw('id,name'));
+
+        return $query;
+    }
+
+    public function getRelated()
+    {
+        return $this->getRelatedQuery()->where('id', '<>', $this->id)->get();
+    }
+}
